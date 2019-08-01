47d9b2b9f5cab848cf8d92c8bd5be56ed5475c11
skypeee <15735184459@163.com>
2019-08-01 19:16:30 +0800

    supplement add function

--- python/05_array/myarray.py
+++ python/05_array/myarray.py
@@ -1,70 +1,46 @@
-#
-# 1) Insertion, deletion and random access of array
-# 2) Assumes int for element type
-#
-# Author: Wenru
-#
-
-
-class MyArray:
-    """A simple wrapper around List.
-    You cannot have -1 in the array.
-    """
-
-    def __init__(self, capacity: int):
+class myArrary(object):
+    def __init__(self, capacity=10):
         self._data = []
         self._capacity = capacity
 
-    def __getitem__(self, position: int) -> object:
+    def __len__(self):
+        return len(self._data)
+    
+    def __getitem__(self, position):
         return self._data[position]
 
-    def __setitem__(self, index: int, value: object):
-        self._data[index] = value
-
-    def __len__(self) -> int:
-        return len(self._data)
-
+    def __setitem__(self, position, value):
+        self._data[position] = value
+    
     def __iter__(self):
         for item in self._data:
             yield item
+    
+    def __str__(self):
+        print(self._data)
 
-    def find(self, index: int) -> object:
-        try:
-            return self._data[index]
-        except IndexError:
-            return None
-
-    def delete(self, index: int) -> bool:
+    def delete(self, index: int):
         try:
             self._data.pop(index)
             return True
         except IndexError:
             return False
 
-    def insert(self, index: int, value: int) -> bool:
+    def insert(self, index, value):
         if len(self) >= self._capacity:
             return False
         else:
-            return self._data.insert(index, value)
+            self._data.insert(index, value)
 
-    def print_all(self):
-        for item in self:
-            print(item)
+    def __add__(self, other):
+        self._data += other._data
+        return self
 
-
-def test_myarray():
-    array = MyArray(5)
-    array.insert(0, 3)
-    array.insert(0, 4)
-    array.insert(1, 5)
-    array.insert(3, 9)
-    array.insert(3, 10)
-    assert array.insert(0, 100) is False
-    assert len(array) == 5
-    assert array.find(1) == 5
-    assert array.delete(4) is True
-    array.print_all()
-
-
-if __name__ == "__main__":
-    test_myarray()
+if __name__== "__main__":
+    a = myArrary()
+    b = myArrary()
+    for i in range(13):
+        a.insert(i, i)
+        b.insert(i, i)
+    a = a + b
+    print(a)
